7d4cd6032ea74db9542f7de4490ce25c6de5d6a8
Ronni Chidekel <ronni@kudo.dk>
2022-05-11 16:00:15 +0200

    add logic for detecting table width

--- table/table.js
+++ table/table.js
@@ -5,14 +5,16 @@ function showTableSwipeInfo() {
 
     // Loop through all tables on the page:
     for (var item of items) {
+        const thisPage = document.querySelector(".table");
+        const hasScrollbar = thisPage.scrollWidth > thisPage.offsetWidth;
+        console.log("thisPage offsetWidth: " + thisPage.offsetWidth);
+        console.log("thisPage scrollWidth: " + thisPage.scrollWidth);
+        console.log("The table is wider than the page? " + hasScrollbar);
+
         item.addEventListener("touchstart", function () {
-
-            this.classList.add(state);
-            // Animations are all handled in css.
-
+            this.classList.add(state); // (Animations are all handled in css).
             // Reset status after 60 seconds:
             setTimeout(_ => this.classList.remove(state), (60*1000));
-
         });
     }
 
