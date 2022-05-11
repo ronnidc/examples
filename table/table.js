49537686da59aa7edea287ebaaf465fa6846a4a6
Ronni Chidekel <ronni@kudo.dk>
2022-05-11 16:23:15 +0200

    add scroll overflow detection

--- table/table.js
+++ table/table.js
@@ -1,25 +1,27 @@
-function showTableSwipeInfo() {
+function responsiveTable() {
 
     const items = document.querySelectorAll(".table");
     const state = "show-swipe-info";
 
     // Loop through all tables on the page:
     for (var item of items) {
-        const thisPage = document.querySelector(".table");
-        const hasScrollbar = thisPage.scrollWidth > thisPage.offsetWidth;
-        console.log("thisPage offsetWidth: " + thisPage.offsetWidth);
-        console.log("thisPage scrollWidth: " + thisPage.scrollWidth);
+        const thisItem = document.querySelector(".table"); // ToDo needs to be uniq for this table and not for all tables on the page
+        const hasScrollbar = thisItem.scrollWidth > thisItem.offsetWidth;
+        console.log("thisItem offsetWidth: " + thisItem.offsetWidth);
+        console.log("thisItem scrollWidth: " + thisItem.scrollWidth);
         console.log("The table is wider than the page? " + hasScrollbar);
 
+        if (thisItem.scrollWidth > thisItem.offsetWidth) {
+            console.log("the table is wider than the page")
+            thisItem.classList.add('table-sticky-column');
+        }
+
+        // ToDo wrap in if column class..
         item.addEventListener("touchstart", function () {
             this.classList.add(state); // (Animations are all handled in css).
-            // Reset status after 60 seconds:
-            setTimeout(_ => this.classList.remove(state), (60*1000));
         });
     }
 
 }
 
-showTableSwipeInfo();
-
-// To Do: "restart" not working properly
+responsiveTable();
