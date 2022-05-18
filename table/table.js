37cf97ff6de610b673ae37333e713d2081a6e44b
Ronni Chidekel <ronni@kudo.dk>
2022-05-18 10:03:23 +0200

    cleanup sticky colums logic

--- table/table.js
+++ table/table.js
@@ -3,23 +3,29 @@ function responsiveTable() {
     const items = document.querySelectorAll(".table");
     const state = "show-swipe-info";
 
-    // Loop through all tables on the page:
-    for (var item of items) {
+    function swipeInfo() {
+        item.addEventListener("touchstart", function () {
+            this.classList.add(state); // (Animation is handled in css).
+        });
+    }
+
+    function stickyColumn() {
         const thisItem = document.querySelector(".table"); // ToDo needs to be uniq for this table and not for all tables on the page
         const hasScrollbar = thisItem.scrollWidth > thisItem.offsetWidth;
         console.log("thisItem offsetWidth: " + thisItem.offsetWidth);
         console.log("thisItem scrollWidth: " + thisItem.scrollWidth);
         console.log("The table is wider than the page? " + hasScrollbar);
 
-        if (thisItem.scrollWidth > thisItem.offsetWidth) {
+        if (hasScrollbar) {
             console.log("the table is wider than the page")
             thisItem.classList.add('table-sticky-column');
+            swipeInfo();
         }
+    }
 
-        // ToDo wrap in if column class..
-        item.addEventListener("touchstart", function () {
-            this.classList.add(state); // (Animations are all handled in css).
-        });
+    // Loop through all tables on the page:
+    for (var item of items) {
+        stickyColumn();
     }
 
 }
