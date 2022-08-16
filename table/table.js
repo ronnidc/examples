cd4a827a93c51b4634f778d80734d706f3bb04f7
Ronni Chidekel <ronni@kudo.dk>
2022-08-16 12:50:02 +0200

    serious refactoring functions & minimize code

--- table/table.js
+++ table/table.js
@@ -1,41 +1,31 @@
 function responsiveTable() {
+
     const items = document.querySelectorAll(".table");
 
-    function stickyColumn() {
-        const elThisItem = document.querySelector(".table"); // ToDo needs to be uniq for THIS table and not for all tables on the page
+    items.forEach(function (item) {
+
         const clSwipeInfo = "show-swipe-info";
         const clStickyColumn = "table-sticky-column";
 
-        function newHeight() {
-            const idealHeight = visualViewport.height * 0.6;
-            elThisItem.style.height = idealHeight + "px";
-        }
-
         function detector() {
-            const wrapperWidth = elThisItem.offsetWidth;
-            const tableWidth = elThisItem.scrollWidth;
-            const overflowX = tableWidth > wrapperWidth;
+            const parentWidth = item.offsetWidth;
+            const tableWidth = item.scrollWidth;
+            const overflowX = tableWidth > parentWidth;
 
             if (overflowX) {
-                newHeight();
-                elThisItem.classList.add(clStickyColumn);
+                item.classList.add(clStickyColumn);
                 item.addEventListener("touchstart", function () {
                     this.classList.add(clSwipeInfo);
                 });
             } else if (!overflowX) {
-                elThisItem.classList.remove(clStickyColumn);
-                elThisItem.style.height = null;
+                item.classList.remove(clStickyColumn);
             }
         }
 
         window.addEventListener("load", detector);
         window.addEventListener('resize', detector);
-    }
 
-    // Loop through all tables on the page:
-    for (var item of items) {
-        stickyColumn();
-    }
+    });
 }
 
 responsiveTable();
