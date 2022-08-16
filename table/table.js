27f9db5ab7cd13f8293fcb80356aa36045d2b932
Ronni Chidekel <ronni@kudo.dk>
2022-08-16 17:42:53 +0200

    make sticky-header optional

--- table/table.js
+++ table/table.js
@@ -2,23 +2,29 @@ function responsiveTable() {
 
     const items = document.querySelectorAll(".table");
 
-    items.forEach(function (item) {
+    const swipeInfoClass = "swipe-info";
+    const stickyHeaderClass = "sticky-header";
+    const stickyColumnClass = "sticky-column";
+    const overflowClass = "no-overflow";
 
-        const swipeInfoClass = "swipe-info";
-        const stickyColumnClass = "sticky-column";
-        const overflowClass = "no-overflow";
+    items.forEach(function (item) {
 
         function detector() {
             const parentWidth = item.offsetWidth;
             const tableWidth = item.scrollWidth;
             const overflowX = tableWidth > parentWidth;
-            const leadingColumn = item.dataset.leadingcolumn;
+            const stickyHeader = item.dataset.stickyheader;
+            const stickyColumn = item.dataset.stickycolumn;
+
+            if (stickyHeader) {
+                item.classList.add(stickyHeaderClass);
+            };
 
             if (overflowX) {
 
                 item.classList.add(overflowClass);
 
-                if (leadingColumn) {
+                if (stickyColumn) {
                     item.classList.add(stickyColumnClass);
                 };
 
@@ -27,9 +33,6 @@ function responsiveTable() {
                 });
 
             } else if (!overflowX) {
-                item.addEventListener("touchstart", function () { // this is stupid but necessary for now.
-                    this.classList.remove(swipeInfoClass);
-                });
                 item.classList.remove(stickyColumnClass, overflowClass, swipeInfoClass);
             };
         }
